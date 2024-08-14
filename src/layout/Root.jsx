5e4ce4d7daf9bb66ff449db366a6020804ba7af3
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-08-15 01:53:26 +0530

    Configured React Router

---
+++ src/layout/Root.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+function Root() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default Root;
